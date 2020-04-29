9fce717d17122195d4d1b75bdf98c1019104baf0
chenkexiu1792 <chenkexiu1792@163.com>
2020-04-29 15:25:51 +0800

    demo init commit

---
+++ src/main/java/com/kexiu/demo/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.kexiu.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
